2b579417f2dc6180476c78b3497a5e629cb66824
kulakov-ad <kulakov-ad@sber-zvuk.com>
2023-01-03 17:37:16 +0300

    make SpaceStampLoader implementation internal

--- main/src/main/java/com/ponykamni/spacestamploader/main/Main.kt
+++ main/src/main/java/com/ponykamni/spacestamploader/main/Main.kt
@@ -1,6 +1,6 @@
 package com.ponykamni.spacestamploader.main
 
-import com.ponykamni.spacestamploader.main.di.DaggerMainComponent
+import com.ponykamni.spacestamploader.main.api.SpaceStampLoader
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
@@ -8,15 +8,11 @@ internal class Main {
 
     companion object {
 
-        private const val LOG_TAG = "com.ponykamni.main.Main"
-
-        private val component = DaggerMainComponent.factory().create()
-
-        private val spaceStampLoader = component.getSpaceStampLoader()
-
         @JvmStatic
         fun main(args: Array<String>): Unit = runBlocking {
             launch {
+
+                val spaceStampLoader = SpaceStampLoader.create()
 
                 spaceStampLoader.getStamps()
             }
--- main/src/main/java/com/ponykamni/spacestamploader/main/api/SpaceStampLoader.kt
+++ main/src/main/java/com/ponykamni/spacestamploader/main/api/SpaceStampLoader.kt
@@ -1,6 +1,14 @@
 package com.ponykamni.spacestamploader.main.api
 
+import com.ponykamni.spacestamploader.main.di.DaggerMainComponent
+
 interface SpaceStampLoader {
 
     suspend fun getStamps(): List<Stamp>
+
+    companion object {
+
+        fun create(): SpaceStampLoader =
+            DaggerMainComponent.factory().create().getSpaceStampLoader()
+    }
 }
